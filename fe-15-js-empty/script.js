e562042b80c1cab0cae80a73d9ab6a0f2aab704c
Sevinch Xatamova <xatamovasevinch@gmail.com>
2023-07-20 19:08:23 +0500

    first commit

---
+++ fe-15-js-empty/script.js
@@ -0,0 +1,52 @@
+// инпут
+let inputFirst = document.querySelector( '.calculator-input-first')
+let inputSecond = document.querySelector('.calculator-input-second')
+
+//кнопки 
+let buttonPlus = document.querySelector('.calculator-plus')
+let buttonMinus = document.querySelector('.calculator-minus')
+let buttonMultiply = document.querySelector('.calculator-multiply')
+let buttonDevide = document.querySelector('.calculator-devide')
+let buttonEquals = document.querySelector('.calculator-equals')
+
+//значения 
+let valueOne = null;
+let valueTwo = null;
+let resut = null;
+
+buttonPlus.addEventListener('click', function() {
+    valueOne = inputFirst.value
+    valueTwo = inputSecond.value
+
+    resut = Number(valueOne) + Number(valueTwo)
+   
+})
+
+buttonMinus.addEventListener('click', function () {
+    valueOne= inputFirst.value
+    valueTwo = inputSecond.value
+
+    resut = Number(valueOne) - Number(valueTwo)
+})
+
+buttonMultiply.addEventListener('click', function () {
+    valueOne = inputFirst.value
+    valueTwo = inputSecond.value
+
+    resut = Number(valueOne) * Number(valueTwo)
+
+})
+
+buttonDevide.addEventListener('clach', function () {
+    valueOne = inputFirst.value
+    valueTwo = inputSecond.value
+
+    resut= Number(valueOne) / Number(valueTwo)
+
+})
+
+buttonEquals.addEventListener('click', function () {
+    alert('результат :' + resut)
+    inputFirst.value = ''
+    inputSecond.value = ''
+} )
